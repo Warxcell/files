93ac231677e5ca5590998868cea3ce17c352e4e3
Bozhidar Hristov <warxcell@gmail.com>
2018-10-11 20:35:09 +0300

    add annotation support for file constraint

--- Validator/Constraint/File.php
+++ Validator/Constraint/File.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Arxy\FilesBundle\Validator\Constraint;
 
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
 class File extends \Symfony\Component\Validator\Constraints\File
 {
 }
