adfbc646007e81a162d29196bed0314172a1036b
Bozhidar Hristov <warxcell@gmail.com>
2018-12-06 11:27:07 +0200

    AppendExtensionStrategy

---
+++ NamingStrategy/AppendExtensionStrategy.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arxy\FilesBundle\NamingStrategy;
+
+use Arxy\FilesBundle\Model\File;
+use Arxy\FilesBundle\NamingStrategy;
+
+class AppendExtensionStrategy implements NamingStrategy
+{
+    /** @var NamingStrategy */
+    private $originalStrategy;
+
+    public function __construct(NamingStrategy $originalStrategy)
+    {
+        $this->originalStrategy = $originalStrategy;
+    }
+
+    public function getDirectoryName(File $file): string
+    {
+        return $this->originalStrategy->getDirectoryName($file);
+    }
+
+    public function getFileName(File $file): string
+    {
+        $extension = pathinfo($file->getOriginalFilename(), PATHINFO_EXTENSION);
+
+        return $this->originalStrategy->getFileName($file).'.'.$extension;
+    }
+}
