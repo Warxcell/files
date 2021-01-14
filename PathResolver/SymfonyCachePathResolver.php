e5e7e3a689407d21473890b9251083127321facc
Warxcell <warxcell@gmail.com>
2021-01-14 16:01:29 +0200

    add symfony cache path resolver.

---
+++ PathResolver/SymfonyCachePathResolver.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Arxy\FilesBundle\PathResolver;
+
+use Arxy\FilesBundle\Model\File;
+use Arxy\FilesBundle\PathResolver;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class SymfonyCachePathResolver implements PathResolver
+{
+    private PathResolver $pathResolver;
+    private CacheInterface $cache;
+    /** @var int|\DateInterval|null $time */
+    private $expiresAfter;
+
+    public function __construct(PathResolver $pathResolver, CacheInterface $cache, $expiresAfter = null)
+    {
+        $this->pathResolver = $pathResolver;
+        $this->cache = $cache;
+        $this->expiresAfter = $expiresAfter;
+    }
+
+    public function getPath(File $file): string
+    {
+        $key = $file->getMd5Hash();
+
+        return $this->cache->get(
+            $key,
+            function (ItemInterface $item) use ($file) {
+                $item->expiresAfter($this->expiresAfter);
+
+                return $this->pathResolver->getPath($file);
+            }
+        );
+    }
+}
