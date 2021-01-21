5e2308e2e73223ed327ca8ab21391ba7deb0085d
Warxcell <warxcell@gmail.com>
2021-01-21 14:55:55 +0200

    use iterable.

--- Repository.php
+++ Repository.php
@@ -9,6 +9,6 @@ interface Repository
 {
     public function findByHashAndSize(string $hash, int $size): ?File;
 
-    /** @return \Generator|File[] */
-    public function findAllForBatchProcessing(): \Generator;
+    /** @return File[] */
+    public function findAllForBatchProcessing(): iterable;
 }
