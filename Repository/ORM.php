a3374ceb9ce1386476a6346d5032a4ec0ffd6b3a
Warxcell <warxcell@gmail.com>
2021-02-23 09:49:37 +0200

    add ORM Repository trait.

---
+++ Repository/ORM.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arxy\FilesBundle\Repository;
+
+
+use Arxy\FilesBundle\Model\File;
+use Doctrine\ORM\QueryBuilder;
+
+trait ORM
+{
+    /** @return QueryBuilder */
+    abstract public function createQueryBuilder($alias, $indexBy = null);
+
+    abstract public function findOneBy(array $criteria, array $orderBy = null);
+
+    public function findByHashAndSize(string $hash, int $size): ?File
+    {
+        return $this->findOneBy(
+            [
+                'md5Hash' => $hash,
+                'fileSize' => $size,
+            ]
+        );
+    }
+
+    public function findAllForBatchProcessing(): iterable
+    {
+        $query = $this->createQueryBuilder('file')->getQuery();
+
+        return $query->toIterable();
+    }
+}
