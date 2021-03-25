6cf776d028370584e8a914d31925a9a94df4275a
Bozhidar Hristov <warxcell@gmail.com>
2021-03-26 00:36:45 +0200

    add tests for AWS S3 Path Resolver.

---
+++ tests/PathResolver/AwsS3PathResolverTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arxy\FilesBundle\Tests\PathResolver;
+
+use Arxy\FilesBundle\ManagerInterface;
+use Arxy\FilesBundle\PathResolver;
+use Arxy\FilesBundle\Tests\File;
+use Aws\S3\S3ClientInterface;
+use PHPUnit\Framework\TestCase;
+
+class AwsS3PathResolverTest extends TestCase
+{
+    private ManagerInterface $manager;
+    private S3ClientInterface $s3Client;
+    private PathResolver\AwsS3PathResolver $pathResolver;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->manager = $this->createMock(ManagerInterface::class);
+        $this->s3Client = $this->createMock(S3ClientInterface::class);
+
+        $this->pathResolver = new PathResolver\AwsS3PathResolver($this->s3Client, 'bucket', $this->manager);
+    }
+
+    public function testGetPath()
+    {
+        $file = new File();
+
+        $this->manager->expects($this->once())->method('getPathname')->with($file)->willReturn('pathname');
+
+        $this->s3Client->expects($this->once())
+            ->method('getObjectUrl')
+            ->with('bucket', 'pathname')
+            ->willReturn('all good');
+
+        $this->assertSame('all good', $this->pathResolver->getPath($file));
+    }
+}
