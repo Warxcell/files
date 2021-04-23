898cebb6af956f3b06b5451357e5e03695ddcfcc
Warxcell <warxcell@gmail.com>
2021-04-24 00:02:41 +0300

    add test for getId and createdAt.

--- tests/Model/DecoratedFileTest.php
+++ tests/Model/DecoratedFileTest.php
@@ -6,18 +6,28 @@ namespace Arxy\FilesBundle\Tests\Model;
 
 use Arxy\FilesBundle\Model\File;
 use Arxy\FilesBundle\Tests\VirtualFile;
+use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 
 class DecoratedFileTest extends TestCase
 {
     private File $decorator;
+    private DateTimeImmutable $createdAt;
 
     public function setUp(): void
     {
         parent::setUp();
 
+        $this->createdAt = new DateTimeImmutable();
         $file = new \Arxy\FilesBundle\Tests\File('filename', 1234, 'md5Hash', 'mimeType');
+        $file->setId(98);
+        $file->setCreatedAt($this->createdAt);
         $this->decorator = new VirtualFile($file);
+    }
+
+    public function testGetId()
+    {
+        $this->assertSame(98, $this->decorator->getId());
     }
 
     public function testGetOriginalFilename()
@@ -62,5 +72,17 @@ class DecoratedFileTest extends TestCase
     {
         $this->decorator->setMimeType('typeMime');
         $this->assertSame('typeMime', $this->decorator->getMimeType());
+    }
+
+    public function testGetCreatedAt()
+    {
+        $this->assertSame($this->createdAt, $this->decorator->getCreatedAt());
+    }
+
+    public function testSetCreatedAt()
+    {
+        $now = new DateTimeImmutable();
+        $this->decorator->setCreatedAt($now);
+        $this->assertSame($now, $this->decorator->getCreatedAt());
     }
 }
