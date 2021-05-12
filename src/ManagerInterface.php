3a72a9f0e71fe29edfa7cd763c80610f23b97a5c
Warxcell <warxcell@gmail.com>
2021-05-12 23:16:55 +0300

    add comments in the interface.

--- src/ManagerInterface.php
+++ src/ManagerInterface.php
@@ -9,23 +9,50 @@ use SplFileInfo;
 
 interface ManagerInterface
 {
+    /**
+     * Converts SplFileInfo instance to file object.
+     */
     public function upload(SplFileInfo $file): File;
 
+    /**
+     * Get underlying path of file
+     */
     public function getPathname(File $file): string;
 
+    /**
+     * Reads the content of file object.
+     */
     public function read(File $file): string;
 
+    /**
+     * @return resource
+     */
     public function readStream(File $file);
 
+    /**
+     * Refreshes mimeType, size and md5 hash from underlying file.
+     */
     public function refresh(File $file): void;
 
+    /**
+     * Rename underlying from $oldStrategy to configured one.
+     */
     public function migrate(File $file, NamingStrategy $oldStrategy): bool;
 
+    /**
+     * Move underlying file to it's final location.
+     */
     public function moveFile(File $file): void;
 
+    /**
+     * Remove underlying file.
+     */
     public function remove(File $file): void;
 
     public function getClass(): string;
 
+    /**
+     * Clears internal FileMap of pending files.
+     */
     public function clear(): void;
 }
