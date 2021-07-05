b86b2c3337f20c2713e8a1a521948b501a0b2993
Bozhidar Hristov <warxcell@gmail.com>
2021-07-06 00:24:23 +0300

    Use FileUtility in NoPreviewGeneratorFound

--- src/Preview/NoPreviewGeneratorFound.php
+++ src/Preview/NoPreviewGeneratorFound.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 namespace Arxy\FilesBundle\Preview;
 
 use Arxy\FilesBundle\Model\File;
+use Arxy\FilesBundle\Utility\FileUtility;
 use RuntimeException;
-use function method_exists;
 
 class NoPreviewGeneratorFound extends RuntimeException
 {
     public static function instance(File $file): self
     {
         return new self(
-            'No preview generator found for file '.
-            (method_exists($file, '__toString') ? (string)$file : (string)spl_object_id($file))
+            sprintf(
+                'No preview generator found for file %s',
+                FileUtility::toString($file)
+            )
         );
     }
 }
